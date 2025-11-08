41b3ffd559a01c0717ea4ad48d40116f6c882a0e
Bohdan Vorobiei <vbs@ukr.net>
2025-11-08 17:04:04 +0200

    The task is almost complete.

--- src/main/java/mate/academy/springboot/web/controller/UserController.java
+++ src/main/java/mate/academy/springboot/web/controller/UserController.java
@@ -1,5 +1,28 @@
 package mate.academy.springboot.web.controller;
 
+
+import mate.academy.springboot.web.model.User;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
 
+    @GetMapping
+    public List<User> getAllUsers() {
+        return List.of(
+                new User(1L, "bob@i.ua"),
+                new User(2L, "alice@i.ua")
+        );
+    }
+
+    @PostMapping
+    public String createUser(@RequestBody User user) {
+        return String.format("User created. Id: %s, email: %s", user.id(), user.email());
+    }
+
 }
+
+
